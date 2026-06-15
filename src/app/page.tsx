82372ca207793730fa1aa8db6de2c712b458adc3
famisics <dev@uiro.dev>
2026-06-15 15:52:03 +0900

    レイアウトを定義

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 export default function Page() {
   return <div>
-    
+    <div>
+      相手の出し手を表示する部分
+    </div>
+    <div>
+      勝敗を表示する部分
+    </div>
+    <div>
+      自分の出し手を表示する部分
+    </div>
+    <div>
+      手のボタンを表示する部分
+    </div>
   </div>;
 }
