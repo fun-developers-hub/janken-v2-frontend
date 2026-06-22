154820837a9998eb1b2321e98c29d6e9d6dda193
noriakiumei <nori2022aki@gmail.com>
2026-06-22 18:12:06 +0900

    CPU,YOUのテキスト、円を１つだけ追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,25 @@ export default function Page() {
         {/* bgはbackground */}
         {/* この環境でh1は意味がないけど、タイトルというマークのためにつけてる */}
       </header>
-      <div className="flex-1 bg-violet-950 text-white">相手の出し手を表示する部分</div>
-      <div className="bg-newblue flex-1">勝敗を表示する部分</div>
-      <div className="flex-1 bg-violet-950 text-white">自分の出し手を表示する部分</div>
-      <div className="h-25 bg-newgreen">手のボタンを表示する部分</div>
+      <div className="flex flex-1 items-center justify-center bg-violet-950 text-white">
+        <div className="size-35 rounded-full bg-pink-700"></div>
+        {/* .circle{
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: white;
+} */}
+      </div>
+      <div className="bg-newblue flex flex-1 flex-col">
+        <div className="text-white">CPU</div>
+        <div className="flex-1 text-center">勝敗を表示する部分</div>
+        <div className="text-right text-white"> YOU</div>
+      </div>
+      <div className="flex-1 bg-violet-950 text-white">
+        <div></div>
+        自分の出し手を表示する部分
+      </div>
+      <div className="bg-newgreen h-25">手のボタンを表示する部分</div>
     </div>
   );
 }
