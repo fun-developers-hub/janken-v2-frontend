bda7203dcd91da5500e624e279147ead285212eb
noriakiumei <nori2022aki@gmail.com>
2026-06-24 11:43:02 +0900

    1-1の実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,14 +17,19 @@ export default function Page() {
       </div>
       <div className="bg-newblue flex flex-1 flex-col">
         <div className="text-white">CPU</div>
-        <div className="flex-1 text-center">勝敗を表示する部分</div>
+        <div className="flex justify-center">
+          <div className="box-content h-32 w-50 border-3 border-green-500 bg-white"></div>
+        </div>
         <div className="text-right text-white"> YOU</div>
       </div>
-      <div className="flex-1 bg-violet-950 text-white">
-        <div></div>
-        自分の出し手を表示する部分
+      <div className="flex flex-1 items-center justify-center bg-violet-950 text-white">
+        <div className="size-35 rounded-full bg-pink-700"></div>
       </div>
-      <div className="bg-newgreen h-25">手のボタンを表示する部分</div>
+      <div className="bg-newgreen flex h-25 items-center justify-center">
+        <div className="m-2 size-20 rounded-full bg-blue-500"></div>
+        <div className="m-2 size-20 rounded-full bg-red-500"></div>
+        <div className="m-2 size-20 rounded-full bg-yellow-300"></div>
+      </div>
     </div>
   );
 }
