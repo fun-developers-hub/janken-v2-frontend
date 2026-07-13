eb30c28f6dc4a3b69b0cadf4735879b198ce31e4
noriakiumei <nori2022aki@gmail.com>
2026-07-13 17:43:55 +0900

    pnpm formatしました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,23 @@ import Image from "next/image";
 
 import { useState } from "react";
 
-
-
 export default function Page() {
-  const [hand,setHand] = useState("");
+  const [hand, setHand] = useState("");
   return (
     <div className="flex h-full flex-col">
       <header>
         <h1 className="bg-newgreen text-center text-4xl">TriGonFight</h1>
         {/* bgはbackground */}
         {/* この環境でh1は意味がないけど、タイトルというマークのためにつけてる */}
-
       </header>
-      <div className="flex flex-1 items-center justify-center bg-violet-950 text-white relative">
+      <div className="relative flex flex-1 items-center justify-center bg-violet-950 text-white">
         <div className="size-35 rounded-full bg-pink-700"></div>
-        <Image src="/images/scissors.png" width={120} height={120} alt="チョキ" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></Image>
+        <Image
+          src="/images/scissors.png"
+          width={120}
+          height={120}
+          alt="チョキ"
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></Image>
         {/* .circle{
   width: 100px;
   height: 100px;
@@ -34,30 +36,28 @@ export default function Page() {
         </div>
         <div className="h-5 text-right text-white"> YOU</div>
       </div>
-      <div className="flex flex-1 items-center justify-center bg-violet-950 text-white relative">
-        {
-          hand ==="rock" ? (
-            <div className="size-35 rounded-full bg-blue-500"></div>
-          ) : hand ==="scissors" ? (
-            <div className="size-35 rounded-full bg-red-500"></div>
-          ) :hand ==="paper" ? (
-            <div className="size-35 rounded-full bg-yellow-300"></div>
-          ) :!hand ?
-        (<div className="size-35 rounded-full bg-pink-700">
-        </div>):null
-    }
-        <div className="absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2">
-          {hand ? (<Image
-            width={120}
-            height={120} 
-            src={`/images/${hand}.png`}
-            alt={hand}></Image>) : null
-          }
-
+      <div className="relative flex flex-1 items-center justify-center bg-violet-950 text-white">
+        {hand === "rock" ? (
+          <div className="size-35 rounded-full bg-blue-500"></div>
+        ) : hand === "scissors" ? (
+          <div className="size-35 rounded-full bg-red-500"></div>
+        ) : hand === "paper" ? (
+          <div className="size-35 rounded-full bg-yellow-300"></div>
+        ) : !hand ? (
+          <div className="size-35 rounded-full bg-pink-700"></div>
+        ) : null}
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          {hand ? (
+            <Image
+              width={120}
+              height={120}
+              src={`/images/${hand}.png`}
+              alt={hand}></Image>
+          ) : null}
         </div>
       </div>
       <div className="bg-newgreen flex h-25 items-center justify-center">
-        <button onClick={() => setHand("rock")} className="relative" >
+        <button onClick={() => setHand("rock")} className="relative">
           <div className="m-2 flex size-20 items-center justify-center rounded-full bg-blue-500"></div>
           <div className="absolute top-1/2 left-1/2 size-20 -translate-x-1/2 -translate-y-1/2">
             <Image
