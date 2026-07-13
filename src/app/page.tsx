1a50a6c0c8903b43d3798eef6fc25a02105ab062
noriakiumei <nori2022aki@gmail.com>
2026-07-13 17:04:50 +0900

    pnpm formatしました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,19 +32,31 @@ export default function Page() {
         <div className="relative">
           <div className="m-2 flex size-20 items-center justify-center rounded-full bg-blue-500"></div>
           <div className="absolute top-1/2 left-1/2 size-20 -translate-x-1/2 -translate-y-1/2">
-            <Image width={100} height={100} src="/images/rock.png" alt="グー"></Image>
+            <Image
+              width={100}
+              height={100}
+              src="/images/rock.png"
+              alt="グー"></Image>
           </div>
         </div>
         <div className="relative">
           <div className="m-2 size-20 rounded-full bg-red-500"></div>
           <div className="absolute top-1/2 left-1/2 size-20 -translate-x-1/2 -translate-y-1/2">
-            <Image width={100} height={100} src="/images/scissors.png" alt="チョキ"></Image>
+            <Image
+              width={100}
+              height={100}
+              src="/images/scissors.png"
+              alt="チョキ"></Image>
           </div>
         </div>
         <div className="relative">
           <div className="m-2 size-20 rounded-full bg-yellow-300"></div>
           <div className="absolute top-1/2 left-1/2 size-20 -translate-x-1/2 -translate-y-1/2">
-            <Image width={100} height={100} src="/images/paper.png" alt="パー"></Image>
+            <Image
+              width={100}
+              height={100}
+              src="/images/paper.png"
+              alt="パー"></Image>
           </div>
         </div>
       </div>
